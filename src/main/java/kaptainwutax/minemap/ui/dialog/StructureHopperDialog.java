2165be4b42e1b37e917d2c13bd99f9680f8bc144
Neil <h66r10@gmail.com>
2021-06-07 05:22:09 +0200

    add it to go to structure also

--- src/main/java/kaptainwutax/minemap/ui/dialog/StructureHopperDialog.java
+++ src/main/java/kaptainwutax/minemap/ui/dialog/StructureHopperDialog.java
@@ -2,9 +2,11 @@ package kaptainwutax.minemap.ui.dialog;
 
 import kaptainwutax.biomeutils.source.BiomeSource;
 import kaptainwutax.featureutils.Feature;
+import kaptainwutax.featureutils.structure.EndCity;
 import kaptainwutax.featureutils.structure.RegionStructure;
 import kaptainwutax.featureutils.structure.Stronghold;
 import kaptainwutax.featureutils.structure.Structure;
+import kaptainwutax.featureutils.structure.generator.structure.EndCityGenerator;
 import kaptainwutax.mcutils.rand.ChunkRand;
 import kaptainwutax.mcutils.state.Dimension;
 import kaptainwutax.mcutils.util.pos.BPos;
@@ -12,11 +14,13 @@ import kaptainwutax.minemap.MineMap;
 import kaptainwutax.minemap.feature.OWBastionRemnant;
 import kaptainwutax.minemap.feature.OWFortress;
 import kaptainwutax.minemap.feature.StructureHelper;
+import kaptainwutax.minemap.init.Features;
 import kaptainwutax.minemap.listener.Events;
 import kaptainwutax.minemap.ui.map.MapContext;
 import kaptainwutax.minemap.ui.map.MapManager;
 import kaptainwutax.minemap.ui.map.MapPanel;
 import kaptainwutax.minemap.ui.map.MapSettings;
+import kaptainwutax.minemap.util.data.Str;
 import kaptainwutax.minemap.util.ui.interactive.Dropdown;
 import kaptainwutax.terrainutils.TerrainGenerator;
 import one.util.streamex.StreamEx;
@@ -24,6 +28,7 @@ import one.util.streamex.StreamEx;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -55,6 +60,18 @@ public class StructureHopperDialog extends Dialog {
             .filter(e -> e instanceof RegionStructure || e instanceof Stronghold/**/)
             .map(e -> new StructureItem((Structure<?, ?>) e))
             .collect(Collectors.toList());
+        if (map.getContext().getDimension()==Dimension.END){
+            structureItems.add(new StructureItem((Structure<?, ?>) Features.getForVersion(map.getContext().getVersion()).get(EndCity.class), bPos -> {
+                EndCityGenerator endCityGenerator = new EndCityGenerator(map.getContext().getVersion());
+                if (!endCityGenerator.generate(map.getContext().getTerrainGenerator(), bPos.toChunkPos())) return false;
+                return endCityGenerator.hasShip();
+            }) {
+                @Override
+                public String toString() {
+                    return "End city with Elytra";
+                }
+            });
+        }
 
         this.structureItemDropdown = new Dropdown<>(structureItems);
         this.continueButton = new JButton();
@@ -69,6 +86,7 @@ public class StructureHopperDialog extends Dialog {
     protected void create() {
         if (!this.continueButton.isEnabled()) return;
         Structure<?, ?> feature = this.structureItemDropdown.getSelected().getFeature();
+        Function<BPos, Boolean> filter = this.structureItemDropdown.getSelected().getFilter();
         if (!(feature instanceof RegionStructure || feature instanceof Stronghold)) return;
         BPos centerPos = manager.getCenterPos();
         BiomeSource biomeSource = context.getBiomeSource();
@@ -84,6 +102,7 @@ public class StructureHopperDialog extends Dialog {
         assert stream != null;
         List<BPos> bPosList = StreamEx.of(stream)
             .sequential()
+            .filter(e -> filter != null ? filter.apply(e) : true)
             .limit(1)
             .collect(Collectors.toList());
         if (!bPosList.isEmpty()) {
@@ -103,9 +122,19 @@ public class StructureHopperDialog extends Dialog {
     static class StructureItem {
 
         private final Structure<?, ?> feature;
+        private final Function<BPos, Boolean> filter;
 
         StructureItem(Structure<?, ?> feature) {
+            this(feature, null);
+        }
+
+        StructureItem(Structure<?, ?> feature, Function<BPos, Boolean> filter) {
             this.feature = feature;
+            this.filter = filter;
+        }
+
+        public Function<BPos, Boolean> getFilter() {
+            return filter;
         }
 
         public Structure<?, ?> getFeature() {
@@ -114,7 +143,7 @@ public class StructureHopperDialog extends Dialog {
 
         @Override
         public String toString() {
-            return feature.getName();
+            return Str.prettifyDashed(feature.getName());
         }
     }
 
