cbd919e08acc0b04d7d06cb77705a98929de5e38
Neil <h66r10@gmail.com>
2021-06-18 03:30:33 +0200

    fix for weird lang

--- src/main/java/kaptainwutax/minemap/config/Config.java
+++ src/main/java/kaptainwutax/minemap/config/Config.java
@@ -1,5 +1,6 @@
 package kaptainwutax.minemap.config;
 
+import com.google.gson.FieldNamingStrategy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import kaptainwutax.minemap.init.Logger;
@@ -8,12 +9,38 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Locale;
 
 import static kaptainwutax.minemap.MineMap.SETTINGS_DIR;
 
 public abstract class Config {
 
+    public static class LocaleAgnosticFieldNamingStrategy implements FieldNamingStrategy {
+        @Override
+        public String translateName(Field field) {
+            return separateCamelCase(field.getName(), "_").toLowerCase(Locale.ROOT);
+        }
+
+        /**
+         * Converts the field name that uses camel-case define word separation into
+         * separate words that are separated by the provided {@code separatorString}.
+         */
+        private static String separateCamelCase(String name, String separator) {
+            StringBuilder translation = new StringBuilder();
+            for (int i = 0; i < name.length(); i++) {
+                char character = name.charAt(i);
+                if (Character.isUpperCase(character) && translation.length() != 0) {
+                    translation.append(separator);
+                }
+                translation.append(character);
+            }
+            return translation.toString();
+        }
+    }
+
     private static final Gson GSON = new GsonBuilder()
+        .setFieldNamingStrategy(new LocaleAgnosticFieldNamingStrategy())
         .excludeFieldsWithoutExposeAnnotation()
         .excludeFieldsWithModifiers(java.lang.reflect.Modifier.TRANSIENT)
         .setPrettyPrinting()
